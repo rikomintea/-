66410bef4c25d15e0ee25e855bd3779312c4c543
Riko Yasuda <rikomintea1827@gmail.com>
2021-05-12 14:24:42 +0900

    Initial Commit

---
+++ アイコン＆実機登録/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  アイコン＆実機登録
+//
+//  Created by スマート・ナビ on 2021/05/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
